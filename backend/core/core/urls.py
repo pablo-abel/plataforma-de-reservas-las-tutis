f0687648fbfe03f780e3c1381645aed4a48ae0d8
pablo-abel <toledopablo037@gmail.com>
2026-06-05 02:33:24 -0300

    Prepara backend para despliegue en produccion

--- backend/core/core/urls.py
+++ backend/core/core/urls.py
@@ -15,16 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('appointments.auth_urls')),
     path('api/appointments/', include('appointments.urls')),
     path('api/gallery/', include('gallery.urls')),
+    # Sirve archivos media de forma directa en desarrollo y producción
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
